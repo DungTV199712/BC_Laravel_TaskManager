ab0b5f6c439f22ce39f1cd89cbd05e45b5b14d51
DungTV <anhdunngcoi@gmail.com>
2018-11-19 14:47:40 +0700

    update

---
+++ resources/views/modules/customer/1.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>id=1</title>
+</head>
+<body>
+<p>1-Nguyễn Văn A-01234567890-email.test@mail.com</p>
+</body>
+</html>
---
+++ resources/views/modules/customer/2.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>id=2</title>
+</head>
+<body>
+<p>2-Nguyễn Văn B-01234567890-email.test@mail.com</p>
+</body>
+</html>
